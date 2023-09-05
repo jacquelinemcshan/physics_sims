7451c65ba8e89cc94bd6dda6ce7896818f82395d
jacquelinemcshan <jacquelinemcshan@gmail.com>
2023-09-04 21:20:30 -0500

    more setup

---
+++ myapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "physicssimulations": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
